43c7e198181df97583cde6a778216989b5a92f13
NATALIE_PODSEDLY <natalie.podsedly@uvm.edu>
2024-10-25 10:28:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log('checking js');
+
+function Candy(name, Reese, Twix, KitKat, GummyBears) {
+    this.name = name;
+    this.newReese = Reese;
+    this.newTwix = Twix;
+    this.newKitKat = KitKat;
+    this.newGummyBear = GummyBear; 
+    this.generateNewCandy = () => {
+        return
+    }
+}
+
+const addCandy = () => {
+    const Candy1 = new Candy('Reese');
+    const Candy1Element = document.getElementById('candy-1');
+    Candy1Element.textContent = candy1.generateNewCandy;
+
+
+}
+
+
+
+
+
